bf4db9144ed2e49e152a87ebac34c6b767b6b1e3
Vage Lulukian <vagggeinsta@yandex.ru>
2022-05-15 16:52:06 +0300

    method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine();
+        System.out.println("Привет " + name);
+
+    }
 }
