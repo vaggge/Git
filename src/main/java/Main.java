58630189d6435109810a00b355a570506de18a78
Vage Lulukian <vagggeinsta@yandex.ru>
2022-05-15 16:56:13 +0300

    mend

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,5 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String name = scan.nextLine();
-        System.out.println("Привет " + name);
-
     }
 }
